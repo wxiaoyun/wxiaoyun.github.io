70d05ca0e45d6612b5144160834be6c4dbd8cf82
ForAeons <w.xy020203@gmail.com>
2024-12-02 12:58:28 +0800

    blog: marathon experience

--- src/schema/medium.ts
+++ src/schema/medium.ts
@@ -3,7 +3,7 @@ import { z } from "astro:content";
 export const MediumItemSchema = z.object({
   title: z.string(),
   pubDate: z.coerce.date(),
-  link: z.string(),
+  link: z.string().optional(),
   thumbnail: z.string().optional(),
   description: z.string(),
   preview: z.string(),
